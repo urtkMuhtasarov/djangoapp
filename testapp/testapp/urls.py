44642affe31e2a0895c3fa5f859ed62fba5519cb
BioUSik <is109-lavrov@urtt.ru>
2022-09-03 14:55:59 +0300

    test commit

--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lr3 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', admin.site.urls),
+    path('', views.homePageView),
 ]
